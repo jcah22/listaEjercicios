d56b13dd6de56c3dc75ef933a23388b03db74f82
julio <jcah549@gmail.com>
2020-06-06 00:28:06 -0500

    pasando datos del componente padre a componente hijo @Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EjerciciosComponent } from './ejercicios/ejercicios.component';
+import {FormsModule} from '@angular/forms';
+import { EjercicioDetalleComponent } from './ejercicio-detalle/ejercicio-detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EjerciciosComponent,
+    EjercicioDetalleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ejercicios/ejercicios.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Ejercicio} from '../ejercicio';
+import {EJERCICIOS} from '../collection-ejercicios';
+
+
+@Component({
+  selector: 'app-ejercicios',
+  templateUrl: './ejercicios.component.html',
+  styleUrls: ['./ejercicios.component.css']
+})
+export class EjerciciosComponent implements OnInit {
+
+  ejercicios = EJERCICIOS;
+  ejercicioSeleccionado: Ejercicio;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSelectEjercicio(ejercicio: Ejercicio): void {
+      console.log('Ejercicio Seleccionado' + ejercicio.id);
+      this.ejercicioSeleccionado = ejercicio;
+  }
+
+}
